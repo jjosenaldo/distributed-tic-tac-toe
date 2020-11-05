a3bd07d416e331cf01e603553178b4ace2e9afcb
gilneyjr <gilneyjnr@gmail.com>
2020-11-05 02:07:05 -0300

    Application is working

--- tictactoe/src/main/java/com/example/tictactoe/ClientApp.java
+++ tictactoe/src/main/java/com/example/tictactoe/ClientApp.java
@@ -1,23 +1,9 @@
 package com.example.tictactoe;
 
 import com.example.tictactoe.controller.ClientController;
-import com.example.tictactoe.controller.IClientController;
 import com.example.tictactoe.gui.GUISwing;
-import com.example.tictactoe.model.exception.ErrorException;
 
 public class ClientApp {
-	public static void test() {
-		IClientController c = new ClientController();
-		try {
-			c.register("za");
-			c.register("za");
-			c.register("ze");
-			c.register("zi");
-		} catch (ErrorException e) {
-			e.printStackTrace();
-		}
-	}
-	
 	public static void main(String[] args) {
 		new GUISwing(new ClientController());
 	}
