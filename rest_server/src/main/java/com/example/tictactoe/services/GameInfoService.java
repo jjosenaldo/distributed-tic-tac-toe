8818b3d7753806af6da83401a6e73cabb16cbb3a
Josenaldo <josenaldo.asj@gmail.com>
2020-11-09 15:44:02 -0300

    Document the /register method

--- rest_server/src/main/java/com/example/tictactoe/services/GameInfoService.java
+++ rest_server/src/main/java/com/example/tictactoe/services/GameInfoService.java
@@ -10,18 +10,18 @@ import com.example.tictactoe.model.response.InfoResponse;
 @Service
 public class GameInfoService {
 	private Game game;
-	
+
 	public GameInfoService() {
 		game = Game.getInstance();
 	}
-	
-    public InfoResponse getGameInfo(String token) {
-    	try {
+
+	public InfoResponse getGameInfo(String token) {
+		try {
 			return InfoResponse.ok(game.getGameInfo(token));
 		} catch (TokenDoesNotExistException e) {
 			return InfoResponse.tokenIsInvalid();
 		} catch (GameHasNotStartedException e) {
 			return InfoResponse.gameHasNotStartedYet();
 		}
-    }
+	}
 }
---
+++ rest_server/src/test/java/com/example/tictactoe/RegisterTest.java
@@ -0,0 +1,87 @@
+package com.example.tictactoe;
+
+import static org.mockito.Mockito.when;
+
+import com.example.tictactoe.controllers.RegisterController;
+import com.example.tictactoe.model.request.RegisterName;
+import com.example.tictactoe.model.response.RegistrationResponse;
+import com.example.tictactoe.services.PlayersService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+
+@WebMvcTest(RegisterController.class)
+@AutoConfigureRestDocs(outputDir = "src/main/asciidoc/target/snippets")
+public class RegisterTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PlayersService service;
+
+    public static String asJsonString(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter objectWriter = mapper.writer().withDefaultPrettyPrinter();
+        return objectWriter.writeValueAsString(object);
+    }
+
+    @Test
+    public void registerOk() throws Exception {
+        when(service.register("jose")).thenReturn(RegistrationResponse.ok("0"));
+
+        RegisterName name = new RegisterName("jose");
+
+        this.mockMvc
+                .perform(post("/register").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(name)))
+                .andExpect(status().isOk())
+                .andDo(document("{methodName}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+                        requestFields(fieldWithPath("name").description("Nome a ser utilizado pelo jogador.")),
+                        relaxedResponseFields(
+                                fieldWithPath("content.token").description("Identificador do jogador."))));
+    }
+
+    @Test
+    public void registerNameAlreadyUsed() throws Exception {
+        when(service.register("jose")).thenReturn(RegistrationResponse.nameAlreadyUsed());
+        RegisterName name = new RegisterName("jose");
+
+        this.mockMvc
+                .perform(post("/register").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(name)))
+                .andExpect(status().isUnprocessableEntity())
+                .andDo(document("{methodName}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())));
+    }
+
+    @Test
+    public void registerNoPlaceAvailable() throws Exception {
+        when(service.register("jose")).thenReturn(RegistrationResponse.noPlaceAvailable());
+        RegisterName name = new RegisterName("jose");
+
+        this.mockMvc
+                .perform(post("/register").contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(name)))
+                .andExpect(status().isServiceUnavailable())
+                .andDo(document("{methodName}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())));
+    }
+}
